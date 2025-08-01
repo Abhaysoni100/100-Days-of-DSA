58dc806438899d9e2702b03e47f7b5f46d314c3c
Abhay Soni <138196874+Abhaysoni100@users.noreply.github.com>
2025-08-01 06:42:49 +0530

    Update WaterBottles.java

--- Day05/WaterBottles.java
+++ Day05/WaterBottles.java
@@ -1,5 +1,4 @@
-/*
-1518. Water Bottles
+/* 1518. Water Bottles
 Solved Easy Topics 
 Hint:  There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water bottles from the market with one full water bottle.
 The operation of drinking a full water bottle turns it into an empty bottle.
